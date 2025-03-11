642d9e74ec45f5d6ec52a8051a641dbe5bc97882
Ryan Strickler <ryanstrickler@Ryans-MacBook-Pro.local>
2025-03-10 22:53:48 -0400

    model complete

--- models/index.js
+++ models/index.js
@@ -6,11 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 
 module.exports = {
   Product,
